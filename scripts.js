c88b9222e32e6b48aea2e18b997d32ae03791a53
Sullivown <daniel.w.t.sullivan@gmail.com>
2021-10-12 16:34:01 +0100

    add sum array

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 // Global variables
 let displayValue = '0';
 let inputs = [];
+let currentSum =[];
 let newInput = true;
-let previousType = null;
 
 // Selector variables
 const output = document.querySelector('#output-div');
@@ -16,98 +16,115 @@ output.textContent = displayValue;
 
 // Operator functions
 function add(a, b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return parseInt(a) - parseInt(b);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return parseInt(a) * parseInt(b);
 }
 
 function divide(a, b) {
-    return a / b;
+    return parseInt(a) / parseInt(b);
 }
 
 function operate(operator, a, b) {
-    return operator(a, b);
+    let funcName = window[operator];
+    return funcName(a, b);
 }
 
 // Helper functions
 function buttonPress() {
     const button = this.dataset.button;
     const buttonType = this.dataset.type;
+    const sumArrayLength = currentSum.length;
 
-    // if a number button
-    if (buttonType === 'number') {
-        // If the previous input was a symbol
-        if (previousType === 'symbol') {
-            // add the symbol to the input array
-            inputs.push(displayValue);
-
-            // clear the display and show the number
-            newInput = true;
-            updateDisplay(button);
-
-        } else {
-            // If the previous input was a number or null
-
-            // append the number to the existing display
-            updateDisplay(button);
-        }
-
-        // Update the previous input variable to reflect the new type
-        previousType = 'number'
-
-    } else {
-        // It is a symbol
-
-        // If the previous input was a number
-        if (previousType === 'number') {
-            // add the number to the input array
-            inputs.push(displayValue);
-
-            // clear the display and show the symbol
-            newInput = true;
-            updateDisplay(button);
-
-        } else if (previousType === 'symbol') {
-            // clear the display and show the symbol
-            newInput = true;
-            updateDisplay(button);
-        } else {
-            // If the previous type is null, just return
-            return;
-        }
-
-        // Update the previous input variable to reflect the new type
-        previousType = 'symbol';
-
-
+    // If all clear is pressed, reset all values and clear the display
+    if (button === 'all-clear') {
+        allClear();
     }
 
-    console.log(inputs)
+    // If flip is pressed, do flip things
+    
+    // If current sum array is empty or has 1 elements:
+    if (!sumArrayLength || sumArrayLength === 1) {
 
-}
+        // if NUMBER pressed:
+        if (buttonType === 'number') {
+            // if first input, set display to number
+            if (newInput) {
+                output.textContent = button;
+                newInput = false;
+            } else {
+                // add number to display
+                output.textContent += button;
+            }
+            displayValue = output.textContent;
+        }
 
+        // if SYMBOL pressed:
+        if (buttonType === 'symbol') {
+            // add display to current sum array[0]
+            currentSum[0] = displayValue;
 
-// Populate the display when inputs detected
-function updateDisplay(button) {
-    if (newInput === true) {
-        displayValue = button;
-        output.textContent = button;
-        newInput = false;
-    } else {
-        displayValue += button;
-        output.textContent += button;
+            // add symbol to current sum array[1]
+            currentSum[1] = button;
+
+            // HIGHLIGHT SYMBOL
+
+            // set new input value
+            newInput = true;
+        }
     }
+
+    // If current sum array length 2 or 3:
+    if (sumArrayLength === 2 || sumArrayLength === 3) {
+        // if NUMBER pressed:
+        if (buttonType === 'number') {
+            // if first input, set display to number
+            if (newInput) {
+                output.textContent = button;
+                newInput = false;
+            } else {
+                // add number to display
+                output.textContent += button;
+            }
+            displayValue = output.textContent;
+        }
+
+        // if SYMBOL pressed:
+        if (buttonType === 'symbol') {
+            currentSum[2] = displayValue;
+            // do the sum
+            let answer = operate(currentSum[1], currentSum[0], currentSum[2]);
+            // output answer to screen
+            displayValue = answer;
+            output.textContent = displayValue;
+            // clear current sum array
+            currentSum = [];
+            // add display to current sum array[0]
+            currentSum[0] = displayValue;
+            // add symbol to current sum array[1]
+            currentSum[1] = button;
+            
+            // set new input value
+            newInput = true;
+
+            // HIGHLIGHT SYMBOL
+        }
+    }
+
+    console.log(currentSum);
 }
 
-// Clear the display and reset all values
-function clear() {
-    displayValue = "0";
+// All clear function
+function allClear() {
+    displayValue = '0';
     inputs = [];
+    currentSum =[];
+    newInput = true;
     output.textContent = displayValue;
 }
